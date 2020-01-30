d1e6eb31c41489aa8cb7cbe010862ce1481e8112
maxloh <35831069+maxloh@users.noreply.github.com>
2020-01-31 01:20:03 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
 touch ~/Templates/Empty\ Document
 sudo apt install gnome-tweaks
-dconf write /org/gnome/desktop/peripherals/mouse/accel-profile 0.6
+dconf load /org/gnome/desktop/peripherals/mouse/ < mouse-settings.txt
+dconf load /org/gnome/shell/extensions/dash-to-panel/ < dash-to-panel-settings.txt
 
 # dash-to-dock
