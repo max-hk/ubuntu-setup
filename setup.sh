05bcd25ca5312e3013ab4ad12381cd5734ac80da
maxloh <35831069+maxloh@users.noreply.github.com>
2020-01-31 04:32:43 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo apt install gnome-tweaks
 sudo apt install dconf-editor
+sudo apt install mate-mouse-properties
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
 touch ~/Templates/Empty\ Document
 dconf load / < dconf.txt
