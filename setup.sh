6d22bfb1e74bec87f0d55d72ed6c541a8dbd4f78
maxloh <35831069+maxloh@users.noreply.github.com>
2020-01-31 04:14:11 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo apt install gnome-tweaks
+sudo apt install dconf-editor
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
 touch ~/Templates/Empty\ Document
 dconf load / < dconf.txt
