6c12c5854e3ee6e32bf51ee5ab6a00af6fb04e62
maxloh <35831069+maxloh@users.noreply.github.com>
2020-01-31 05:28:44 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 sudo apt install gnome-tweaks
 sudo apt install dconf-editor
-sudo apt install mate-mouse-properties
+sudo apt-get install Imwheel
+# sudo apt install mate-mouse-properties
+
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
 touch ~/Templates/Empty\ Document
 dconf load / < dconf.txt
-
-# dash-to-panel
