c3e5d3abed57a5f1288d0e374530e3df53d05108
maxloh <35831069+maxloh@users.noreply.github.com>
2020-01-31 01:23:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
+sudo apt install gnome-tweaks
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
 touch ~/Templates/Empty\ Document
-sudo apt install gnome-tweaks
 dconf load / < dconf.txt
 
-# dash-to-dock
+# dash-to-panel
