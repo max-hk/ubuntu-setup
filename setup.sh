7e10a27cbd2bf521dbb7e2d444e2a315e3fea7b5
maxloh <35831069+maxloh@users.noreply.github.com>
2020-02-16 05:35:43 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo apt install chrome-gnome-shell
 sudo apt install dconf-editor
 sudo apt install imwheel
 sudo apt install ibus-cangjie
+sudo apt install fonts-firacode
 
 cp -r ./~/. ~/
 touch ~/Templates/Empty\ Document
