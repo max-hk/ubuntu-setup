6298f80c0cc752da3d50471cf97fbf5ccf384a31
maxloh <35831069+maxloh@users.noreply.github.com>
2020-02-21 04:15:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ sudo apt install --yes dconf-editor
 sudo apt install --yes imwheel
 sudo apt install --yes ibus-cangjie
 
+# Amazon shortcut
+sudo apt remove --yes ubuntu-web-launchers
+# Aisleriot Solitaire game
+sudo apt remove --yes aisleriot
+
 cp -r ./~/. ~/
 touch ~/Templates/Empty\ Document
 dconf load / < dconf.txt
